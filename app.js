fff984fcc08023a8dfcf4d2f3e0361e457f36c4f
N000173276 <benson.ye@npc.com.tw>
2022-10-03 14:00:52 +0800

    S2U47: Build project

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const PORT = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 
 
